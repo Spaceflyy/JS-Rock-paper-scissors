066605a9d64f9fb954517e8dc1c1e55362e852bf
Spaceflyy <115657060+Spaceflyy@users.noreply.github.com>
2022-12-16 15:04:30 +0000

    Add playRound function to determine result

--- game.js
+++ game.js
@@ -1,27 +1,74 @@
-getComputerChoice();
+
+const computerChoice = getComputerChoice();
+
+function playRound(playerSelection, computerSelection)
+{
+    let winner;
+
+    playerSelection = playerSelection.toLowerCase();
+    computerSelection = computerSelection.toLowerCase();
+
+    if (playerSelection === computerSelection)
+    {
+        winner = "Its a draw!";
+
+    }   
+    else if (playerSelection === "rock") 
+    {
+        if (computerSelection === "paper")
+        {
+            winner ="You lose! Paper covers Rock!";
+        }   
+        else 
+        {
+            winner ="You win! Paper covers Rock!";
+        }
+    }
+    else if(playerSelection === "paper")
+    {
+        if (computerSelection ==="scissors")
+        {
+            winner ="You lose! Scissors cuts Paper!";
+        } 
+        else
+        {
+            winner ="You Win! Scissors cuts Paper!";
+        }   
+    } 
+    else if(playerSelection === "scissors")
+    {
+        if (computerSelection === "rock")
+        {
+            winner = "You Lose! Rock smashes Scissors!";
+        }
+        else
+        {
+            winner = "You win! Rock smashes Sciccors!";
+        }
+    }
+    return winner;
+}
+
 
 function getComputerChoice()
 {
     //declare computerchoice variable
-    let computerChoice;
+    let choice;
     let randomNum;
-    randomNum = Math.floor((Math.random()* 3) + 1);
     // generate random number between 1 and 3
+    randomNum = Math.floor((Math.random()* 3) + 1);
+ 
+    // if 1 then rock if 2 paper if 3 scissors
     if (randomNum === 1)
     {
-        computerChoice = "Rock";
+        choice = "Rock";
     } else if (randomNum === 2)
     {
-        computerChoice = "Paper";
+        choice = "Paper";
 
     } else 
     {
-        computerChoice = "Scissors";
+        choice = "Scissors";
     }
-
-    console.log(computerChoice);
-    
-    // if 1 then rock if 2 paper if 3 scissors
-    // assign result to variable 
-    // return computer choice
+    return choice;
 }
