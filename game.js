eb2d96d66979e1fb548aca481bcd20886cfd388b
Spaceflyy <115657060+Spaceflyy@users.noreply.github.com>
2022-12-16 14:12:32 +0000

    Create getComputerChoice function

--- game.js
+++ game.js
@@ -1,3 +1,27 @@
-console.log("hello world!");
+getComputerChoice();
 
+function getComputerChoice()
+{
+    //declare computerchoice variable
+    let computerChoice;
+    let randomNum;
+    randomNum = Math.floor((Math.random()* 3) + 1);
+    // generate random number between 1 and 3
+    if (randomNum === 1)
+    {
+        computerChoice = "Rock";
+    } else if (randomNum === 2)
+    {
+        computerChoice = "Paper";
 
+    } else 
+    {
+        computerChoice = "Scissors";
+    }
+
+    console.log(computerChoice);
+    
+    // if 1 then rock if 2 paper if 3 scissors
+    // assign result to variable 
+    // return computer choice
+}
